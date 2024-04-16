fbcb95f336a4f151e4ba062388a6a34944e7b204
aayushshuk1a <aayush5emails@gmail.com>
2024-04-17 00:38:30 +0530

    Added REST config

--- Employee Rest-API/EmployeeRestAPI_usingDataRest/src/main/java/com/employeerestapi/EmployeeRestAPI_usingDataRest/dao/EmployeeRepository.java
+++ Employee Rest-API/EmployeeRestAPI_usingDataRest/src/main/java/com/employeerestapi/EmployeeRestAPI_usingDataRest/dao/EmployeeRepository.java
@@ -2,8 +2,10 @@ package com.employeerestapi.EmployeeRestAPI_usingDataRest.dao;
 
 import com.employeerestapi.EmployeeRestAPI_usingDataRest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 
+
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
 }
