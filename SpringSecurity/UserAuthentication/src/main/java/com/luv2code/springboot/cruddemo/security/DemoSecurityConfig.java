5d1385f164addc7e6e1a925369487cb113191185
aayushshuk1a <aayush5emails@gmail.com>
2024-04-17 22:00:03 +0530

    Added Role Restriction

--- SpringSecurity/UserAuthentication/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ SpringSecurity/UserAuthentication/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -2,9 +2,13 @@ package com.luv2code.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -27,10 +31,31 @@ public class DemoSecurityConfig {
         UserDetails hailey= User.builder()
                 .username("hailey")
                 .password("{noop}test123")
-                .roles("MANGAER","EMPLOYEE","ADMIN")
+                .roles("MANAGER","EMPLOYEE","ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(john,mary,hailey);
+
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception{
+        httpSecurity.authorizeHttpRequests(configurer->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/employees/**").hasRole(("EMPLOYEE"))
+                        .requestMatchers(HttpMethod.POST,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/employees/**").hasRole("ADMIN")
+        );
+
+        //Http Basic Authentication
+        httpSecurity.httpBasic(Customizer.withDefaults());
+
+        httpSecurity.csrf(csrf->csrf.disable());
+
+        return httpSecurity.build();
     }
 
 }
