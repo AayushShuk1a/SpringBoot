632744feb98b58f9cb224c81446fba035131777d
aayushshuk1a <aayush5emails@gmail.com>
2024-04-19 20:40:24 +0530

    Added Free Passes

--- Thymeleaf/CustomerValidationForm/src/main/java/com/validationdemo/CustomerValidationForm/Customer.java
+++ Thymeleaf/CustomerValidationForm/src/main/java/com/validationdemo/CustomerValidationForm/Customer.java
@@ -1,6 +1,8 @@
 package com.validationdemo.CustomerValidationForm;
 
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -13,6 +15,18 @@ public class Customer {
     @NotNull(message = "is Required")
     @Size(min = 1,message = "Last Name Should not be empty")
     private String lastName="";
+
+    @Min(value = 1,message = "Number should be greater than 1")
+    @Max(value = 10,message = "Number should be less than 10")
+    private int freePasses;
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
