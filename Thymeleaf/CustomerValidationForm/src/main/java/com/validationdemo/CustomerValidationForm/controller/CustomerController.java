8c0df28c7f79eb9d8c73757d3614f4c6398a6808
aayushshuk1a <aayush5emails@gmail.com>
2024-04-19 19:52:30 +0530

    Init Binder

--- Thymeleaf/CustomerValidationForm/src/main/java/com/validationdemo/CustomerValidationForm/controller/CustomerController.java
+++ Thymeleaf/CustomerValidationForm/src/main/java/com/validationdemo/CustomerValidationForm/controller/CustomerController.java
@@ -3,15 +3,25 @@ package com.validationdemo.CustomerValidationForm.controller;
 
 import com.validationdemo.CustomerValidationForm.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor=new StringTrimmerEditor(true);
+
+        webDataBinder.registerCustomEditor(String.class,stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model theModel){
