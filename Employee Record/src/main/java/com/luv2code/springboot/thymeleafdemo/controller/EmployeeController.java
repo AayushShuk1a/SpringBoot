187e01465f36c42b2cad3e6eadadc5f8b07ec2e0
aayushshuk1a <aayush5emails@gmail.com>
2024-04-20 19:38:31 +0530

    EmployeeRecord Update Employees

--- Employee Record/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ Employee Record/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -21,6 +23,13 @@ public class EmployeeController {
 		this.employeeService=employeeService;
 	}
 
+	@GetMapping("/showformadd")
+	public String ShowFormToAdd(Model theModel){
+		Employee employee=new Employee();
+		theModel.addAttribute("employee",employee);
+		return "employee/employee-form";
+	}
+
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 
@@ -30,8 +39,17 @@ public class EmployeeController {
 		theModel.addAttribute("employees",employeeList);
 
 
-		return "list-employees";
+		return "employee/list-employees";
 	}
+
+
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employees")Employee employee){
+		employeeService.save(employee);
+
+		return "redirect:/employees/list";
+	}
+
 }
 
 
