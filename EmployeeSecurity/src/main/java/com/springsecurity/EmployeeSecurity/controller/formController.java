6175699a4bea77791ce8fe7e3596087fc6ab6e94
aayushshuk1a <aayush5emails@gmail.com>
2024-04-22 02:40:22 +0530

    Employee Login Security

--- EmployeeSecurity/src/main/java/com/springsecurity/EmployeeSecurity/controller/formController.java
+++ EmployeeSecurity/src/main/java/com/springsecurity/EmployeeSecurity/controller/formController.java
@@ -1,15 +1,13 @@
 package com.springsecurity.EmployeeSecurity.controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class demoController {
-
-    @GetMapping("/")
-    public String Home(){
-        return "Home";
+public class formController {
+    @GetMapping("/showLoginPage")
+    public String LoginForm(){
+        return "plain-form";
     }
-
 }
---
+++ EmployeeSecurity/src/main/java/com/springsecurity/EmployeeSecurity/security/demoSecurityConfig.java
@@ -0,0 +1,46 @@
+package com.springsecurity.EmployeeSecurity.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class demoSecurityConfig {
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john= User.builder()
+                .username("john")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary=User.builder()
+                .username("mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+        UserDetails susan=User.builder()
+                .username("susan")
+                .password("{noop}test123")
+                .roles("ADMIN","MANAGER","EMPLOYEE")
+                .build();
+
+        return  new InMemoryUserDetailsManager(john,mary,susan);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer->configurer.anyRequest().authenticated())
+                .formLogin(form->form.loginPage("/showLoginPage")
+                        .loginProcessingUrl("/authenticateUser")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
+}
