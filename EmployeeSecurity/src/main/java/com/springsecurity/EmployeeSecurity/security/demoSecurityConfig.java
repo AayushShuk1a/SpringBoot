1208825414ec5f82026b6998df247adb51da2025
aayushshuk1a <aayush5emails@gmail.com>
2024-04-22 03:40:55 +0530

    Custom Login Form

--- EmployeeSecurity/src/main/java/com/springsecurity/EmployeeSecurity/security/demoSecurityConfig.java
+++ EmployeeSecurity/src/main/java/com/springsecurity/EmployeeSecurity/security/demoSecurityConfig.java
@@ -4,13 +4,17 @@ package com.springsecurity.EmployeeSecurity.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class demoSecurityConfig {
+
+    @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john= User.builder()
                 .username("john")
@@ -32,6 +36,7 @@ public class demoSecurityConfig {
 
         return  new InMemoryUserDetailsManager(john,mary,susan);
     }
+
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
